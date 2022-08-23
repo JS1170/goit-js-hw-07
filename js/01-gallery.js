7916a4f80a46e00a0bedab973fac645eff444b9d
Yevgen Savchuk <107425775+JS1170@users.noreply.github.com>
2022-08-23 15:59:36 +0300

    tasks 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
+console.log(galleryItems);
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const imgList = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', imgList);
+console.log(gallery);
+
+gallery.addEventListener('click', handleClick);
+  const instance = basicLightbox.create(`
+    <img width="800" height="600">
+`);
+
+function handleClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const dataSource = event.target.dataset.source;
+
+  instance.element().querySelector('img').src = dataSource;
+  instance.show();
+
+  gallery.addEventListener('keydown', closeEsc);
+
+  function closeEsc(event) {
+    event.preventDefault();
+    if (event.key === 'Escape') {
+      instance.close();
+      gallery.removeEventListener('keydown', closeEsc);
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const imgList = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`;
+  })
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', imgList);
+ let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+console.log(SimpleLightbox);
+
+function handleClick(event) {
+  event.preventDefault();
+
+ 
+}
